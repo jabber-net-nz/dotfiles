1515fed7a97cc4b78871e5b8f7bea6186208d692
Leon Strong <leon.strong@oss-group.co.nz>
2026-02-07 11:41:26 +1300

    added terraformrc

--- backup.sh
+++ backup.sh
@@ -2,9 +2,14 @@
 
 LOGFILE=$HOME/winhome/backup/${WSL_DISTRO_NAME}.log
 SRC_DIR=/home/leons
-DEST_DIR=/mnt/Default/Virtuals/backups
-SERVER=truenas.dyn.homenet
-# rsync SRC to DEST on SERVER
+DEST_DIR_A=/mnt/Default/Virtuals/backups
+DEST_DIR_B=/mnt/virtuals/backups
+SERVERA=truenas.dyn.homenet
+SERVERB=trunx.dyn.homenet
 
-rsync --progress -av $SRC_DIR/ lstrong@$SERVER:$DEST_DIR/$WSL_DISTRO_NAME/
+# rsync SRC to DEST on SERVER
+echo "Copying to truenas"echo "Copying to truenas"
+rsync --progress -av --delete $SRC_DIR/ lstrong@$SERVERA:$DEST_DIR_A/$WSL_DISTRO_NAME/
+echo "Copying to trunx"
+rsync --progress -av --delete $SRC_DIR/ leons@$SERVERB:$DEST_DIR_B/$WSL_DISTRO_NAME/
 
